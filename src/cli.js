cfe9848874e9a6259a7ad1148fadea1092af9494
caique <kaikejrsilva55@gmail.com>
2024-07-17 22:46:12 -0300

    Aprendendo as variaveis de Node.js

---
+++ src/cli.js
@@ -0,0 +1,73 @@
+import fs from 'fs';
+import path from 'path';
+import trataErros from './erros/funcoesErro.js';
+import { contaPalavras } from './index.js';
+import { montaSaidaArquivo } from './helpers.js';
+import { Command } from 'commander';
+import chalk from 'chalk';
+
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .option('-t, --texto <string>', 'caminho do texto a ser processado')
+  .option('-d, --destino <string>', 'caminho da pasta onde salvar o arquivo de resultados')
+  .action((options) => {
+    const { texto, destino } = options;
+
+    if (!texto || !destino) {
+      console.error(chalk.red('erro: favor inserir caminho de origem e destino'))
+      program.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log(chalk.green('texto processado com sucesso'));
+    } catch (erro) {
+      console.log('ocorreu um erro no processamento', erro);
+    }
+  })
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, 'utf-8', (erro, texto) => {
+    try {
+      if (erro) throw erro
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino)
+    } catch(erro) {
+      trataErros(erro);
+    }
+  })
+}
+
+
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
+  try {
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log('arquivo criado');
+  } catch(erro) {
+    throw erro;
+  }
+}
+
+// function criaESalvaArquivo(listaPalavras, endereco) {
+//   const arquivoNovo = `${endereco}/resultado.txt`;
+//   const textoPalavras = JSON.stringify(listaPalavras);
+  
+//   fs.promises.writeFile(arquivoNovo, textoPalavras)
+//     .then(() => {
+//       console.log('arquivo criado');
+//     })
+//     .catch((erro) => {
+//       throw erro
+//     })
+//     .finally(() => console.log('operação finalizada'))
+// }
---
+++ src/index.js
@@ -0,0 +1,30 @@
+
+export function contaPalavras(texto) {
+    const paragrafos = extraiParagrafos(texto)
+    const contagem = paragrafos.flatMap((paragrafo) => {
+      if (!paragrafo) return [];
+      return verificaPalavrasDuplicadas(paragrafo);
+    })
+    return contagem;
+  }
+  
+  function extraiParagrafos(texto) {
+    return texto.toLowerCase().split('\n');
+  }
+  
+  function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+  }
+  
+  function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+    listaPalavras.forEach(palavra => {
+      if (palavra.length >= 3) {
+        const palavraLimpa = limpaPalavras(palavra);
+        resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+      }
+    })
+    return resultado;
+  }
+  
